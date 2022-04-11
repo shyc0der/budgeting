b77453942f626f15ea15f9da5c8fd82a6c3f46f1
shyc0der <wachirasteady@gmail.com>
2022-04-11 07:56:17 +0300

    status

--- lib/src/pages/budgetCategoryPage/budgetCategoryPage.dart
+++ lib/src/pages/budgetCategoryPage/budgetCategoryPage.dart
@@ -19,54 +19,57 @@ class _BudgetCategoryPageState extends State<BudgetCategoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-        body: Padding(
-          
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              StreamBuilder<List<BudgetCategoryModel>>(
-                  stream: _budgetCategoryModule.fetchBudgets(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return const LinearProgressIndicator();
-                      case ConnectionState.none:
-                        return const Text('No data');
-                      default:
-                        return ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: (snapshot.data ?? []).length,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemBuilder: (BuildContext context, int index) {
-                              return GestureDetector(
-                                onTap: () {
-                                                    
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => AddBudgetCategory(
-                                          isEditing: true,
-                                          budget: snapshot.data?[index],
-                                        )));
-                          },
-                                child: ListTile(
-                                  title: Text(snapshot.data![index].name ?? ''),
-                                  //DateTime.parse(snapshot.data![index].month.toString()).month.toString()
-                                  leading: Text(DateFormat("MMMM").format(
-                                      DateTime.parse(snapshot.data![index].month
-                                          .toString()))),
-                                  subtitle: Text(
-                                      'Ksh. ${snapshot.data![index].amountBudgeted ?? ''}'),
-                                  trailing: Text(DateTime.parse(
-                                          snapshot.data![index].month.toString())
-                                      .year
-                                      .toString()),
-                                ),
-                              );
-                            });
-                    }
-                  }),
-            ],
+        body: SingleChildScrollView(
+          //physics:const  NeverScrollableScrollPhysics(),
+          child: Padding(
+            
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                StreamBuilder<List<BudgetCategoryModel>>(
+                    stream: _budgetCategoryModule.fetchBudgets(),
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.waiting:
+                          return const LinearProgressIndicator();
+                        case ConnectionState.none:
+                          return const Text('No data');
+                        default:
+                          return ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: (snapshot.data ?? []).length,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemBuilder: (BuildContext context, int index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                                      
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => AddBudgetCategory(
+                                            isEditing: true,
+                                            budget: snapshot.data?[index],
+                                          )));
+                            },
+                                  child: ListTile(
+                                    title: Text(snapshot.data![index].name ?? ''),
+                                    //DateTime.parse(snapshot.data![index].month.toString()).month.toString()
+                                    leading: Text(DateFormat("MMMM").format(
+                                        DateTime.parse(snapshot.data![index].month
+                                            .toString()))),
+                                    subtitle: Text(
+                                        'Ksh. ${snapshot.data![index].amountBudgeted ?? ''}'),
+                                    trailing: Text(DateTime.parse(
+                                            snapshot.data![index].month.toString())
+                                        .year
+                                        .toString()),
+                                  ),
+                                );
+                              });
+                      }
+                    }),
+              ],
+            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
--- lib/src/pages/monthlyIncomePage/addMonthlyIncome.dart
+++ lib/src/pages/monthlyIncomePage/addMonthlyIncome.dart
@@ -9,137 +9,153 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AddMonthlyIncome extends StatefulWidget {
-  const AddMonthlyIncome({ Key? key }) : super(key: key);
+  const AddMonthlyIncome({Key? key}) : super(key: key);
 
   @override
   _AddMonthlyIncomeState createState() => _AddMonthlyIncomeState();
 }
 
 class _AddMonthlyIncomeState extends State<AddMonthlyIncome> {
-  TextEditingController salaryController=TextEditingController();
-  TextEditingController extraIncomeController=TextEditingController();
-  final MonthlyIncomeModule _monthlyIncomeModule= MonthlyIncomeModule();
-  
-  bool nameError=false;
-  bool amountError=false;
-  bool isLoading=false;
-  bool errorExists=false;
+  TextEditingController salaryController = TextEditingController();
+  TextEditingController extraIncomeController = TextEditingController();
+  final MonthlyIncomeModule _monthlyIncomeModule = MonthlyIncomeModule();
+
+  bool nameError = false;
+  bool amountError = false;
+  bool isLoading = false;
+  bool errorExists = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-      const    BackgroundPage(),
-       Padding(
+          const BackgroundPage(),
+          Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[  
-                const SizedBox(height: 30,),  
-                //icon
-              IconButton(onPressed: (){
-              Navigator.of(context).pop();
-            }, icon: const  Icon(Icons.arrow_back,color: Colors.black87, size: 27,)),
-           
-               const SizedBox(height: 60,),           
-                //Text
-                const Text('ADD MONTHLY INCOME',         
-                 style: TextStyle(fontWeight: FontWeight.bold,fontSize: 23,color: Colors.black),),                
-               const SizedBox(height: 20,),        
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              const SizedBox(
+                height: 30,
+              ),
+              //icon
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.black87,
+                    size: 27,
+                  )),
+
+              const SizedBox(
+                height: 60,
+              ),
+              //Text
+              const Text(
+                'ADD MONTHLY INCOME',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23,
+                    color: Colors.black),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               //name
-              inputField('MONTHLY INCOME', salaryController, nameError,isNumbers: true,
-              onChanged: (val){
-                if (val.isNotEmpty){
+              inputField('MONTHLY INCOME', salaryController, nameError,
+                  isNumbers: true, onChanged: (val) {
+                if (val.isNotEmpty) {
                   setState(() {
-                    nameError=false;
+                    nameError = false;
                   });
                 }
-              }
+              }),
+              const SizedBox(
+                height: 10,
               ),
-               const SizedBox(height: 10,), 
               //Amount Budgeted
-              inputField('EXTRA INCOME ', extraIncomeController, amountError,isNumbers: true,
-              onChanged: (val) {
-                if(val.isNotEmpty){
-                   setState(() {
-                     amountError=false;
-                   });
-                }
-              },
-              ),   
+              inputField(
+                'EXTRA INCOME ',
+                extraIncomeController,
+                amountError,
+                isNumbers: true,
+                onChanged: (val) {
+                  if (val.isNotEmpty) {
+                    setState(() {
+                      amountError = false;
+                    });
+                  }
+                },
+              ),
 
               //TODO: MONTH
-              const  SizedBox(height:36),
-             //Save
-             Row(
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               children:[
-           
-            Align(
-              
-                 alignment: Alignment.bottomLeft,
-                   child: CustomElavtedButton(
-                      errorExists: errorExists,
-                     label: 'CANCEL',
-                     fontSize: 15,
-                     onTap: (){
+              const SizedBox(height: 36),
+              //Save
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: CustomElavtedButton(
+                    errorExists: errorExists,
+                    label: 'CANCEL',
+                    fontSize: 15,
+                    onTap: () {
                       Navigator.of(context).pop();
                     },
-                   ), ), 
-                   Align(
-                     
-                 alignment: Alignment.bottomRight,
-                   child: CustomElavtedButton(
-                     errorExists: errorExists,
-                     label: 'ADD BUDGET',
-                     fontSize: 15,
-                     onTap: (){
-                      if(salaryController.text.isEmpty){
-                        setState((){
-                          nameError=true;
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: CustomElavtedButton(
+                    errorExists: errorExists,
+                    label: 'ADD BUDGET',
+                    fontSize: 15,
+                    onTap: () {
+                      if (salaryController.text.isEmpty) {
+                        setState(() {
+                          nameError = true;
                         });
-                        errorExists=true;
-                      }if(extraIncomeController.text.isEmpty){
-                        setState((){
-                          amountError=true;
-                        });
-                        errorExists=true;
+                        errorExists = true;
                       }
-                      if(!errorExists){
-                        addBudget();
+                      if (extraIncomeController.text.isEmpty) {
+                        setState(() {
+                          amountError = true;
+                        });
+                        errorExists = true;
+                      }
+                      if (!errorExists) {
+                        addIncome();
                       }
                     },
-                   ),),
-          
-              
-               ]
-             )
-             
-              ]
-            ),
+                  ),
+                ),
+              ])
+            ]),
           ),
         ],
       ),
-      
     );
   }
-  void addBudget()async{
+
+  void addIncome() async {
     setState(() {
-      isLoading=true;
+      isLoading = true;
     });
-    var res=await _monthlyIncomeModule.addIncome(MonthlyIncomeModel(
-      salary : salaryController.text ,
-      extraIncome: extraIncomeController.text,
-      createdBy: 'Steady',
-      dateCreated: DateTime.now()
-      
-    ));
-    if (res.status == ResponseType.success){
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Income Added Sucessfully')));
+    var res = await _monthlyIncomeModule.addIncome(MonthlyIncomeModel(
+        salary: salaryController.text,
+        extraIncome: extraIncomeController.text,
+        createdBy: 'Steady',
+        dateCreated: DateTime.now()));
+    if (res.status == ResponseType.success) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Income Added Sucessfully')));
       Navigator.pop(context);
-    } 
-    else{
-      Get.showSnackbar(GetSnackBar(title: 'Error',message: res.body.toString(), backgroundColor: Colors.redAccent,));
+    } else {
+      Get.showSnackbar(GetSnackBar(
+        title: 'Error',
+        message: res.body.toString(),
+        backgroundColor: Colors.redAccent,
+      ));
     }
   }
 }
