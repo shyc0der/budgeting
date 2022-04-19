48fca4b59fb9c72a33e379cd4602ddc69886676e
shyc0der <wachirasteady@gmail.com>
2022-04-19 22:37:36 +0300

    budget

--- lib/src/pages/main.dart
+++ lib/src/pages/main.dart
@@ -18,23 +18,41 @@ class _FirstPageState extends State<FirstPage> {
          const BackgroundPage(), 
          Padding(
            padding: const EdgeInsets.all(10.0),
+           
            child: Column(
              crossAxisAlignment: CrossAxisAlignment.start,
              children: [
+               const SizedBox(
+                height: 100,
+              ),
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.black87,
+                    size: 27,
+                  )),
+                  const SizedBox(
+                height: 50,
+              ),
                Expanded(
-                 child: GridView(
-                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-                   children: [
-                      Image.asset('assets/images/budget.png',height: 30,
-                       width: 30,),
-                       Image.asset('assets/images/budget.png',height: 30,
-                       width: 30,),
-                       Image.asset('assets/images/budget.png',height: 30,
-                       width: 30,)
-                   ],
-                   ),
+                 child: Wrap(
+                           children: [
+                        Column(
+                          children: [
+                            Image.asset('assets/images/budget.png',width: 200,),
+                            Text('Budget',style: TextStyle(fontWeight: FontWeight.bold),)
+                          ],
+                        ),
+                         Image.asset('assets/images/budget.png', width: 200,),
+                         Image.asset('assets/images/budget.png',width: 200,)
+                     ],
+                     ),
+                 ),
 
-                )
+                
              ],
            ),
          )
