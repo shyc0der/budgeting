3ca70f15a251f897e24e5b8844731db700288f93
shyc0der <wachirasteady@gmail.com>
2022-04-19 23:28:41 +0300

    page

--- lib/src/pages/main.dart
+++ lib/src/pages/main.dart
@@ -1,9 +1,13 @@
+import 'package:budget/src/pages/budgetCategoryPage/budgetCategoryPage.dart';
+import 'package:budget/src/pages/expense/expensePage.dart';
+import 'package:budget/src/pages/monthlyIncomePage/monthlyIncomePage.dart';
 import 'package:flutter/material.dart';
+
 
 import 'background_page.dart';
 
 class FirstPage extends StatefulWidget {
-  const FirstPage({ Key? key }) : super(key: key);
+  const FirstPage({Key? key}) : super(key: key);
 
   @override
   State<FirstPage> createState() => _FirstPageState();
@@ -13,16 +17,14 @@ class _FirstPageState extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children :[
-         const BackgroundPage(), 
-         Padding(
-           padding: const EdgeInsets.all(10.0),
-           
-           child: Column(
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: [
-               const SizedBox(
+      body: Stack(children: [
+        const BackgroundPage(),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
                 height: 100,
               ),
               IconButton(
@@ -34,31 +36,98 @@ class _FirstPageState extends State<FirstPage> {
                     color: Colors.black87,
                     size: 27,
                   )),
-                  const SizedBox(
-                height: 50,
+              const SizedBox(
+                height: 40,
               ),
-               Expanded(
-                 child: Wrap(
-                           children: [
-                        Column(
-                          children: [
-                            Image.asset('assets/images/budget.png',width: 200,),
-                            Text('Budget',style: TextStyle(fontWeight: FontWeight.bold),)
-                          ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20,bottom: 10),
+                  child: Wrap(
+                    spacing: 20.0,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        BudgetCategoryPage()));
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                'assets/images/budget.png',
+                                width: 100,
+                              ),
+                              const Text(
+                                'BUDGET',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
                         ),
-                         Image.asset('assets/images/budget.png', width: 200,),
-                         Image.asset('assets/images/budget.png',width: 200,)
-                     ],
-                     ),
-                 ),
-
-                
-             ],
-           ),
-         )
-        ]
-      ),
-      
+                      ),
+                      const SizedBox(
+                        width: 20,
+                        height: 40,
+                      ),
+                      Padding(                       
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const ExpensesPage()));
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                'assets/images/budget.png',
+                                width: 100,
+                              ),
+                              const Text(
+                                'EXPENSES',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20, height: 40),
+                      Padding(                        
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const  MonthlyIncomePage()));
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                'assets/images/budget.png',
+                                width: 100,
+                              ),
+                              const Text(
+                                'INCOMES',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
