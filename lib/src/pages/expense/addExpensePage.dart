403e78a918deab78d2f9090bb826f1cb32bd3c90
shyc0der <wachirasteady@gmail.com>
2022-05-19 23:29:34 +0300

    commit

--- lib/src/pages/expense/addExpensePage.dart
+++ lib/src/pages/expense/addExpensePage.dart
@@ -23,7 +23,7 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
- final ExpenseModule _expenseModule = ExpenseModule();
+  final ExpenseModule _expenseModule = ExpenseModule();
   late String selectedDate = DateTime.now().toString();
   TextEditingController expenseTypeController = TextEditingController();
   TextEditingController expenseAmountController = TextEditingController();
@@ -32,8 +32,8 @@ class _AddExpenseState extends State<AddExpense> {
   bool errorExists = false;
   bool isLoading = false;
 
-UserModule userModel = Get.put(UserModule());
-    
+  UserModule userModel = Get.put(UserModule());
+
   final BudgetCategoryModule _budgetCategoryModule = BudgetCategoryModule();
   List<Map<String, dynamic>> expenseTypesAmount = [
     {
@@ -55,184 +55,194 @@ UserModule userModel = Get.put(UserModule());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown[50],
+        backgroundColor: Colors.brown[50],
         body: Stack(
-      children: [
-        const BackgroundPage(),
-        Padding(
-            padding: const EdgeInsets.all(10),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const SizedBox(
-                height: 30,
-              ),
-              IconButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    color: Colors.black87,
-                    size: 27,
-                  )),
-              const SizedBox(
-                height: 50,
-              ),
-              //Text
-              Text(
-                widget.isEditing ? 'EDIT EXPENSE' : 'ADD EXPENSE',
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 23,
-                    color: Colors.black),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              //Date
-              DateTimePicker(
-                decoration:const  InputDecoration(fillColor: Color.fromRGBO(85, 54, 33, 1),),
-                initialValue: selectedDate.toString(),
-                firstDate: DateTime(2022),
-                lastDate: DateTime(2122),
-                dateLabelText: 'Date',
-                onChanged: (value) {
-                  setState(() {
-                    selectedDate = value;
-                  });
-                },
-                initialDatePickerMode: DatePickerMode.year,
-              ),
-
-              //expense type
-              inputField('Expense Type', expenseTypeController, isExpenseType,
-                  onChanged: (val) {
-                if (val.isEmpty) {
-                  setState(() {
-                    isExpenseType = true;
-                  });
-                }
-              }),
-              //amount
-              inputField(
-                'Amount',
-                expenseAmountController,
-                isAmount,
-                isNumbers: true,
-                onChanged: (val) {
-                  if (val.isEmpty) {
-                    setState(() {
-                      isAmount = true;
-                    });
-                  }
-                },
-              ),
-
-            const  SizedBox(
-                height: 10,
-              ),
-              ElevatedButton.icon(
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color.fromRGBO(85, 54, 33, 1),
-                  ),
-                  onPressed: () async {
-                    if (expenseAmountController.text.isEmpty) {
-                      setState(() {
-                        isAmount = true;
-                      });
-                      errorExists = true;
-                    }
-                    if (expenseTypeController.text.isEmpty) {
-                      setState(() {
-                        isExpenseType = true;
-                      });
-                      errorExists = true;
-                    }
-
-                    if (!errorExists) {
-                      addItemsToList();
-                    }
-                  },
-                  icon: const Icon(Icons.add),
-                  label: const Text('Add Expenses')),
-
-              const SizedBox(
-                height: 12,
-              ),
-              //list view builder
-
-              expenseTypesAmount.isNotEmpty
-                  ? 
-                  Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(3.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text('Expenses'),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            Expanded(
-                              child: ListView.builder(
-                                  itemCount: expenseTypesAmount.length,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return SingleChildScrollView(
-                                      child: Card(
-                                        child: Dismissible(
-                                          key: UniqueKey(),
-                                          onDismissed: (_) async{
-                                            
-                                            await _expenseModule
-                                                .deleteExpenseExpenses(
-                                                    (widget.expenseModel!.id!),
-                                                    index.toString());
-                                                    
-                                          },
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text(
-                                                expenseTypesAmount[index]
-                                                        ['item']['expense']
-                                                    .toString(),
-                                              ),
-                                              Text(expenseTypesAmount[index]
-                                                      ['item']['amount']
-                                                  .toString())
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  }),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: CustomElavtedButton(
-                                errorExists: errorExists,
-                                label: 'Save Expenses',
-                                fontSize: 15,
-                                onTap: () {
-                                  widget.isEditing
-                                      ? editExpenses()
-                                      : addExpenses();
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
+          children: [
+            const BackgroundPage(),
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 30,
                       ),
-                    )
-                  : Container()
-            ])),
-      ],
-    ));
+                      IconButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.black87,
+                            size: 27,
+                          )),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      //Text
+                      Text(
+                        widget.isEditing ? 'EDIT EXPENSE' : 'ADD EXPENSE',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 23,
+                            color: Colors.black),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      //Date
+                      DateTimePicker(
+                        decoration: const InputDecoration(
+                          fillColor: Color.fromRGBO(85, 54, 33, 1),
+                        ),
+                        initialValue: selectedDate.toString(),
+                        firstDate: DateTime(2022),
+                        lastDate: DateTime(2122),
+                        dateLabelText: 'Date',
+                        onChanged: (value) {
+                          setState(() {
+                            selectedDate = value;
+                          });
+                        },
+                        initialDatePickerMode: DatePickerMode.year,
+                      ),
+
+                      //expense type
+                      inputField(
+                          'Expense Type', expenseTypeController, isExpenseType,
+                          onChanged: (val) {
+                        if (val.isEmpty) {
+                          setState(() {
+                            isExpenseType = true;
+                          });
+                        }
+                      }),
+                      //amount
+                      inputField(
+                        'Amount',
+                        expenseAmountController,
+                        isAmount,
+                        isNumbers: true,
+                        onChanged: (val) {
+                          if (val.isEmpty) {
+                            setState(() {
+                              isAmount = true;
+                            });
+                          }
+                        },
+                      ),
+
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color.fromRGBO(85, 54, 33, 1),
+                          ),
+                          onPressed: () async {
+                            if (expenseAmountController.text.isEmpty) {
+                              setState(() {
+                                isAmount = true;
+                              });
+                              errorExists = true;
+                            }
+                            if (expenseTypeController.text.isEmpty) {
+                              setState(() {
+                                isExpenseType = true;
+                              });
+                              errorExists = true;
+                            }
+
+                            if (!errorExists) {
+                              addItemsToList();
+                            }
+                          },
+                          icon: const Icon(Icons.add),
+                          label: const Text('Add Expenses')),
+
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      //list view builder
+
+                      expenseTypesAmount.isNotEmpty
+                          ? Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text('Expenses'),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Expanded(
+                                      child: ListView.builder(
+                                          itemCount: expenseTypesAmount.length,
+                                          itemBuilder: (BuildContext context,
+                                              int index) {
+                                            return SingleChildScrollView(
+                                              child: Card(
+                                                child: Dismissible(
+                                                  key: UniqueKey(),
+                                                  onDismissed: (_) async {
+                                                    expenseTypesAmount
+                                                        .removeAt(index);
+                                                        
+                                                    await _expenseModule
+                                                        .deleteExpenseExpenses(
+                                                            (widget
+                                                                .expenseModel!
+                                                                .id!),
+                                                            index.toString());
+                                                  },
+                                                  child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceBetween,
+                                                    children: [
+                                                      Text(
+                                                        expenseTypesAmount[
+                                                                        index]
+                                                                    ['item']
+                                                                ['expense']
+                                                            .toString(),
+                                                      ),
+                                                      Text(expenseTypesAmount[
+                                                                  index]['item']
+                                                              ['amount']
+                                                          .toString())
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                            );
+                                          }),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.bottomRight,
+                                      child: CustomElavtedButton(
+                                        errorExists: errorExists,
+                                        label: 'Save Expenses',
+                                        fontSize: 15,
+                                        onTap: () {
+                                          widget.isEditing
+                                              ? editExpenses()
+                                              : addExpenses();
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          : Container()
+                    ])),
+          ],
+        ));
   }
 
   void addExpenses() async {
@@ -249,6 +259,12 @@ UserModule userModel = Get.put(UserModule());
       ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Expenses Added Sucessfully')));
       Navigator.pop(context);
+      //print(0)
+      //print(0)
+      //print(0)
+      //print(0)
+      //print(0)
+      //print(0)
     } else {
       Get.showSnackbar(GetSnackBar(
         title: 'Error',
